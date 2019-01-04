94344caedfa73f8796361407dc794ab63f0c8a03
IamMonkie <monkie_225@yahoo.com>
2019-01-04 11:49:28 -0500

    changed http --> https, fixed unauthenticated script error

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,31 +1,31 @@
-$(document).ready(function () {
+$(document).ready(function() {
   createButton();
   goButton();
 
   $("#buttonContainer").on("click", ".animal-btn", function(event) {
-    console.log("hit")
+    console.log("hit");
     event.preventDefault();
 
     // user Input
     let animal = $(this).attr("data-name");
     // Create Query URL
-    let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=etI6oymnFWc2axcUGIrsEv4jRn32lOtK&limit=10";
-    
+    let queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      animal +
+      "&api_key=etI6oymnFWc2axcUGIrsEv4jRn32lOtK&limit=10";
+
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {      
-
+    }).then(function(response) {
       let results = response.data;
       var animalDiv = $("<div>");
-      
+
       // $("#gifContainer").prepend(animalImage);
 
       for (let i = 0; i < results.length; i++) {
-
         // Only taking action if the photo has an appropriate rating
         if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-
           let p = $("<p>").text("Rating: " + results[i].rating);
           let animalImage = $("<img>");
 
@@ -36,32 +36,29 @@ $(document).ready(function () {
 
           // Prepending the gifDiv to the gifContainer in the HTML
           $("#gifContainer").prepend(gifDiv);
-          
+
           console.log("YAY", gifDiv);
         }
       }
     });
   });
 
-  $("#gifContainer").on("click", "#animalImage", function(){
-    console.log("clicked " + animalImage)
+  $("#gifContainer").on("click", "#animalImage", function() {
+    console.log("clicked " + animalImage);
     let state = $(this).attr("data-state");
     let source = $(this).html("#gifDiv");
 
-    if(state === 'still'){
-
+    if (state === "still") {
       let dataAnimate = $(this).attr("data-animate");
       $(this).attr("src", dataAnimate);
-      $(this).attr("data-state", 'animate');
+      $(this).attr("data-state", "animate");
+    } else if (state === "animate") {
+      let dataStill = $(this).attr("data-still");
+      $(this).attr("src", dataStill);
+      $(this).attr("data-state", "still");
     }
-    else if (state === 'animate') {
-
-      let dataStill = $(this).attr('data-still');
-      $(this).attr('src', dataStill);
-      $(this).attr('data-state', 'still');
-    }
-  })
-}) // document.ready close
+  });
+}); // document.ready close
 
 //--------------------------------------------------------------------------
 // functions
@@ -70,16 +67,12 @@ let gifs = ["dog", "cat", "birb", "ferret", "monkey"];
 let animalImage = $("<img>");
 let gifDiv = $("<div>");
 
-
-
 function createButton() {
+  $("#buttonContainer").empty();
 
-$("#buttonContainer").empty();
-
-for (let i = 0; i < gifs.length; i++) {
-  
-  let a = $("<button>");
-    a.addClass("animal-btn");   
+  for (let i = 0; i < gifs.length; i++) {
+    let a = $("<button>");
+    a.addClass("animal-btn");
     a.attr("data-name", gifs[i]);
     a.text(gifs[i]);
     $("#buttonContainer").append(a);
@@ -88,15 +81,15 @@ for (let i = 0; i < gifs.length; i++) {
 
 // go button
 function goButton() {
+  $("#searchButton").on("click", function(event) {
+    event.preventDefault();
+    let animal = $("#animalInput")
+      .val()
+      .trim();
+    console.log(animal);
+    gifs.push(animal);
 
-$("#searchButton").on("click", function(event) {
-  event.preventDefault();
-  let animal = $("#animalInput").val().trim();
-  console.log(animal)
-  gifs.push(animal);
-
-  // Calling renderButtons which handles the processing of our array
-  createButton();
-
-}); 
-};
+    // Calling renderButtons which handles the processing of our array
+    createButton();
+  });
+}
